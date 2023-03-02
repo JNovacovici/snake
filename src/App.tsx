058332b6b89c7582d0ac367b6af7230123cb94c0
Jason <jason.novacovici@gmail.com>
2023-03-01 19:02:55 -0500

    initial snake setup of react hooks, variables, function names

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import AppleLogo from './apple.png';
+import useInterval from './useInterval';
+
+const boardX = 1000;
+const boardY = 1000;
+const initialSnake = [[4, 10], [4, 10]];
+const initialApple = [14, 10];
+const scale = 50;
+const timeDelay = 100;
+
 
 function App() {
+  const canvasRef = useRef(null);
+  const [snake, setSnake] = useState(initialSnake);
+  const [apple, setApple] = useState(initialApple);
+  const [direction, setDirection] = useState([0, -1]);
+  const [delay, setDelay] = useState<number | null>(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
+
+  useInterval(() => runGame(), delay);
+
+  function runGame() {
+    const newSnake = [...snake];
+    const newSnakeHead= [newSnake[0][0] + direction[0], newSnake[0][1] + direction[1]]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onKeyDown={(e) => changeDirection(e)}>
+      <img src={AppleLogo} alt="snakeFood" width="30"/>
+      <canvas className="playArea" ref={canvasRef} width={`${boardX}px`} height={`${boardY}px`}/>
+      {gameOver && <div className="gameOver"> Game Over </div>}
+      <button onClick={startGame} className="startButton"> Play </button>
+      <div className ="scoreList">
+        <h2>Score: {score}</h2>
+        <h2>High Score: {localStorage.getItem("snakeScore")}</h2>
+      </div>
     </div>
   );
 }
